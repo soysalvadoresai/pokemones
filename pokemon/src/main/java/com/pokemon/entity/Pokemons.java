cc66cb9376d61828f401b0b58a786f35773a6a31
Chava <69120088+soysalvadoresai@users.noreply.github.com>
2022-02-21 12:20:39 -0600

    Add files via upload
    Package creation and entity

---
+++ pokemon/src/main/java/com/pokemon/entity/Pokemons.java
@@ -0,0 +1,15 @@
+package com.pokemon.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Entity
+@Data
+@Table(name = "Pokemons")
+public class Pokemons {
+
+}
